1924a88123a11f18173db4b7c8a5d1f3470b8698
olegkomlev44 <74656349+olegkomlev44@users.noreply.github.com>
2026-06-21 09:24:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -174,7 +174,7 @@ async def handle_ai_chat(message: types.Message):
             tools=[{"google_search": {}}] 
         )
         USER_CHATS[user_id] = gemini_client.aio.chats.create(
-            model='gemini-2.5-flash',
+            model='gemini-3.5-flash',
             config=config
         )
 
